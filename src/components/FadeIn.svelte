a32e14030a5d9235406fd58433e28ec4c8b52432
jdobrowner <jdobrowner@gmail.com>
2024-11-08 14:52:23 +0100

    fade in cmp

---
+++ src/components/FadeIn.svelte
@@ -0,0 +1,14 @@
+<script>
+  import { cubicInOut } from 'svelte/easing';
+  import { fade } from 'svelte/transition';
+
+  export let duration = 1000;
+</script>
+
+<style>
+  /* No additional styles needed for the fade transition */
+</style>
+
+<div transition:fade={{ duration, easing: cubicInOut }}>
+  <slot></slot>
+</div>
